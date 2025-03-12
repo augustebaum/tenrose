eb019bf9ea7ff0f022eecdaf504b77038b934051
Auguste Baum <auguste.apple@gmail.com>
2025-03-12 18:20:13 +0100

    Add wasm32-wasip1 target to rust install

--- shell.nix
+++ shell.nix
@@ -1,11 +1,17 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
-  pkgs = import nixpkgs {};
+  rust-overlay = fetchTarball "https://github.com/oxalica/rust-overlay/archive/7af16cbd1464fddde8ad0c4ed7baaa2292445ba4.tar.gz";
+
+  pkgs = import nixpkgs {overlays = [(import rust-overlay)];};
 in
   pkgs.mkShell {
     packages = with pkgs; [
       # To compile to WASM
       cargo
+      (rust-bin.stable.latest.default.override {
+        extensions = [];
+        targets = ["wasm32-wasip1"];
+      })
       lld
 
       # To download penrose/core
